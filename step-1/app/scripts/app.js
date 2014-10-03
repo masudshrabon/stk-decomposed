a06d51c1a3850dda2bf1c42321000ac5ab1bf5f8
Diego Netto <diegormnetto@gmail.com>
2014-10-03 17:49:18 -0500

    Initial Commit

---
+++ step-1/app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name stockDogApp
+ * @description
+ * # stockDogApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('stockDogApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ]);
