c06f31a13489e832e4f57e9191d5c97457377fb1
Diego Netto <diegormnetto@gmail.com>
2014-11-02 15:35:31 -0600

    Scaffolded dashboard and watchlist routes using Yeoman sub-generator

--- step-3/app/scripts/app.js
+++ step-3/app/scripts/app.js
@@ -20,6 +20,14 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
+      })
+      .when('/watchlist/:listId', {
+        templateUrl: 'views/watchlist.html',
+        controller: 'WatchlistCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
