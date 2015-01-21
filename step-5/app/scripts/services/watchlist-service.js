494eb2ea1357db4030242548a17df199a3c579a2
Diego Netto <diegormnetto@gmail.com>
2015-01-21 06:23:36 -0600

    Step-5 first pass

--- step-5/app/scripts/services/watchlist-service.js
+++ step-5/app/scripts/services/watchlist-service.js
@@ -2,14 +2,63 @@
 
 angular.module('stockDogApp')
   .service('WatchlistService', function WatchlistService() {
+    // Augment Stocks with additional helper functions
+    var StockModel = {
+      save: function () {
+        var watchlist = findById(this.listId);
+        watchlist.recalculate();
+        saveModel();
+      }
+    };
+
+    // Augment Watchlists with additional helper functions
+    var WatchlistModel = {
+      addStock: function (stock) {
+        var existingStock = _.find(this.stocks, function (s) {
+          return s.company.symbol === stock.company.symbol;
+        });
+        if (existingStock) {
+          existingStock.shares += stock.shares;
+        } else {
+          _.extend(stock, StockModel);
+          this.stocks.push(stock);
+        }
+        this.recalculate();
+        saveModel();
+      },
+      removeStock: function (stock) {
+        _.remove(this.stocks, function (s) {
+          return s.company.symbol === stock.company.symbol;
+        });
+        this.recalculate();
+        saveModel();
+      },
+      recalculate: function () {
+        var calcs = _.reduce(this.stocks, function (calcs, stock) {
+          calcs.shares += stock.shares;
+          calcs.marketValue += stock.marketValue;
+          calcs.dayChange += stock.dayChange;
+          return calcs;
+        }, { shares: 0, marketValue: 0, dayChange: 0 });
+
+        this.shares = calcs.shares;
+        this.marketValue = calcs.marketValue;
+        this.dayChange = calcs.dayChange;
+      }
+    };
+
     // Helper: Load watchlists from localStorage
     var loadModel = function () {
       var model = {
-        watchlists: localStorage['StockDog.watchlists'] ?
-          JSON.parse(localStorage['StockDog.watchlists']) : [],
-        nextId: localStorage['StockDog.nextId'] ?
-          parseInt(localStorage['StockDog.nextId']) : 0
+        watchlists: localStorage['StockDog.watchlists'] ? JSON.parse(localStorage['StockDog.watchlists']) : [],
+        nextId: localStorage['StockDog.nextId'] ? parseInt(localStorage['StockDog.nextId']) : 0
       };
+      _.each(model.watchlists, function (watchlist) {
+        _.extend(watchlist, WatchlistModel);
+        _.each(watchlist.stocks, function (stock) {
+          _.extend(stock, StockModel);
+        });
+      });
       return model;
     };
 
@@ -38,6 +87,8 @@ angular.module('stockDogApp')
     // Save a new watchlist to watchlists model
     this.save = function (watchlist) {
       watchlist.id = Model.nextId++;
+      watchlist.stocks = [];
+      _.extend(watchlist, WatchlistModel);
       Model.watchlists.push(watchlist);
       saveModel();
     };
